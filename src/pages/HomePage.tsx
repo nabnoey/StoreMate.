b8a79ece6da66aa8016a41b1bd01cf2833456b8d
nabnoey <nabnoeyy@gmail.com>
2026-02-12 18:47:58 +0700

    shoppage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -11,11 +11,11 @@ function HomePage() {
     <div className="w-full mt-6 flex flex-col items-center">
 
  
-      <div className="carousel w-2/3 h-full">
+      <div className="w-full h-full  overflow-hidden">
         <div className="carousel-item relative w-full">
           <img
             src="https://scontent.fbkk12-5.fna.fbcdn.net/v/t39.30808-6/475814878_1153008693494064_4029239289864672932_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=cc71e4&_nc_eui2=AeGBV730DK8906AMrEp2-PRifP9NtagBx0R8_021qAHHRE8Ee1AcZueGpR8eXLOC6JJunUiPRGXoKwofnyOzCFqn&_nc_ohc=tTKwayP65ZcQ7kNvwE5WkjA&_nc_oc=AdkLghdC_rI-kOJxC4rAzyBnHvntr4YNbypziCzqRe04bnSXuhx1H8Fu4WiFw30xH7ALYtN8XUqb_-deMUBJ6QNb&_nc_zt=23&_nc_ht=scontent.fbkk12-5.fna&_nc_gid=Enp-gqcadvRpGmEAOSWtJg&oh=00_Afu-me6QDroAuNMgkFHP7Z0Vl9b4JYH3QvWDQskIQ6EA3g&oe=699279CA"
-            className="w-full h-full object-contain"
+            className="w-full h-full object-cover"
           />
         </div>
       </div>
