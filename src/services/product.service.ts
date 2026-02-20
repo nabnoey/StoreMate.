1432d7ccfcf765a9c8ecb04adb940f06d766ba89
nabnoey <nabnoeyy@gmail.com>
2026-02-20 21:41:42 +0700

    Refactor product service methods and rename search function
    Signed-off-by: nabnoey <nabnoeyy@gmail.com>

--- src/services/product.service.ts
+++ src/services/product.service.ts
@@ -1,11 +1,5 @@
 import api from "./api";
-// import type { Product } from "../types/product";
 
-
-//  const getAllProducts = async () => {
-//     const res = await api.get(`${import.meta.env.VITE_PRODUCT_API}/products`)
-//     return res.data
-// }
 
  const getAllCategories = async () => {
     const res = await api.get(`${import.meta.env.VITE_PRODUCT_API}/grouped-by-category`)
@@ -17,14 +11,14 @@ import api from "./api";
     return res.data
 }
 
- const getProductBySearch = async (query: string) => {
-    const res = await api.get(`${import.meta.env.VITE_PRODUCT_API}/search?query=${query}`)
+const searchProducts = async (keyword: string) => {
+    const res = await api.get(`${import.meta.env.VITE_PRODUCT_API}/search?keyword=${keyword}`)
     return res.data
 }
 
 export const ProductService = {
-    // getAllProducts,
+   
     getAllCategories,
     getProductById,
-    getProductBySearch
+  searchProducts
 }
