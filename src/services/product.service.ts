3122c022be1b8c25ff59f85ae9a0599dffc2a22b
nabnoey <nabnoeyy@gmail.com>
2026-03-15 12:28:20 +0700

    edit productReducer

--- src/services/product.service.ts
+++ src/services/product.service.ts
@@ -9,8 +9,8 @@ const searchProducts = async ( keyword: string,
   category: string,
   minPrice?: number,
   maxPrice?: number,
-  size?:1,
-  page?:1000
+  size?:number,
+  page?:number
  ) => {
     const res = await api.get(`${import.meta.env.VITE_PRODUCT_API}/search?keyword=${encodeURIComponent(keyword)}&category=${category}&minPrice=${minPrice}&maxPrice=${maxPrice}&size=${size}&page=${page}`)
     return res.data
