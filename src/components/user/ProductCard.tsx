789a3be99f1f76031a692ecbd053d72eafc8be9c
nabnoey <nabnoeyy@gmail.com>
2026-03-15 15:08:49 +0700

    Edit ProductCard

--- src/components/user/ProductCard.tsx
+++ src/components/user/ProductCard.tsx
@@ -1,69 +1,58 @@
 import { Link } from "react-router-dom";
-import {useState, useEffect} from "react"
-import { ProductService } from "../../services/product.service";
-import type { Product} from "../../types/product"
+import type { Product } from "../../types/product";
 
-
-
-
-type Props = {
-  product: Product;
-};
+type Props = { product: Product };
 
 function ProductCard({ product }: Props) {
-const [description, setDescription] = useState<string>("กำลังโหลดข้อมูล...");
-
-useEffect(() => {
-    const fetchDetail = async () => {
-      try {
-        const detailData = await ProductService.getProductById(product.id);
-        setDescription(detailData.description || "ไม่มีคำอธิบายสินค้า");
-      } catch (error) {
-        console.error("ดึงข้อมูล description ไม่สำเร็จ", error);
-        setDescription(""); 
-      }
-    };
-
-    fetchDetail();
-  }, [product.id]);
   return (
-          <Link to={`/product/${product.id}`} state={{ categoryName: product.categoryName }} className="block group" data-test="product-card">
-    <div className="card bg-white rounded-2xl shadow-md hover:shadow-xl transition-shadow duration-300 p-3 text-black w-full max-w-[280px] h-[450px] relative">
+    <Link
+      to={`/product/${product.id}`}
+      state={{ categoryName: product.categoryName }}
+      className="block group"
+      data-test="product-card"
+    >
 
+      
+      <div className="card bg-white rounded-2xl shadow-md hover:shadow-xl transition-shadow duration-300 p-3 text-black w-full max-w-[280px] h-[450px] relative">
         <figure className="px-2 pt-2 overflow-hidden rounded-2xl h-[250px]">
           <img
-            src={product.imageUrl || "https://scontent.fbkk12-1.fna.fbcdn.net/v/t39.30808-6/631033255_1486282403500023_4710477623864277946_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=13d280&_nc_ohc=LVsLjxBcDngQ7kNvwFmpYeP&_nc_oc=AdmHGAm1Ibg5tetmmBOuVUnoW_F2a1qp7KhZsXxMvcnSR7A5c33a3gZ1xUjWiQ_TpjoNQHOLqHy16moZpzcR1Kzo&_nc_zt=23&_nc_ht=scontent.fbkk12-1.fna&_nc_gid=byhROHe1c6lbVBOBQwjGhw&oh=00_AfvmSssPV69WDuHi2p-gcgpsU1WcdQhqEid0bw71o-2qmQ&oe=699E715D"}
+            src={
+              product.imageUrl ||
+              "https://scontent.fbkk12-1.fna.fbcdn.net/v/t39.30808-6/631033255_1486282403500023_4710477623864277946_n.jpg"
+            }
             alt={product.productName}
             className="w-full h-full object-cover rounded-xl transition-transform duration-500 group-hover:scale-110"
           />
         </figure>
         <div className="card-body p-4 flex flex-col justify-between">
+    
           <div>
+        
             <h2
               className="card-title text-base w-full whitespace-normal break-words line-clamp-2 group-hover:text-blue-600 transition-colors"
               title={product.productName}
             >
+              
               {product.productName}
             </h2>
             <p className="text-sm text-gray-500 line-clamp-2 mt-2">
-              {description}
+             
+              {product.description || "ไม่มีคำอธิบายสินค้า"}
             </p>
-          </div> 
+          </div>
         </div>
-   
-
-      <div className="px-4 pb-4 mt-auto">
-        <div className="flex justify-between items-center">
-          <p className="font-extrabold text-lg text-blue-500">
-            ฿{product.price}
-          </p>
-
+        <div className="px-4 pb-4 mt-auto">
          
+          <div className="flex justify-between items-center">
+            
+            <p className="font-extrabold text-lg text-blue-500">
+             
+              ฿{product.price}
+            </p>
+          </div>
         </div>
       </div>
-    </div>
-       </Link>
+    </Link>
   );
 }
-
 export default ProductCard;
