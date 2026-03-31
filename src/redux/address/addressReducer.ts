82ceed040200482a43e94cb0804046b3238cf827
nabnoey <nabnoeyy@gmail.com>
2026-03-31 11:56:55 +0700

    edit address

--- src/redux/address/addressReducer.ts
+++ src/redux/address/addressReducer.ts
@@ -1,59 +1,38 @@
-import { ADD_ADDRESS, DELETE_ADDRESS, SET_DEFAULT_ADDRESS } from "./actionTypes";
-import type { AddressAction, AddAddressAction, DeleteAddressAction, SetDefaultAddressAction } from "./addressAction";
-import { addressInitialState, type AddressState } from "./addressInitialState";
-import type { UnknownAction } from "redux";
+import {createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { UserService } from "../../services/users.service";
+import type { Address } from "../../types/address";
 
-const addressReducer = (
-  state = addressInitialState,
-  action: AddressAction | UnknownAction
-): AddressState => {
-  switch (action.type) {
-    case ADD_ADDRESS: {
-      const newAddress = (action as AddAddressAction).payload;
-      return {
-        ...state,
-        address: [...state.address, newAddress], // เอาของเก่ามากาง แล้วต่อท้ายด้วยของใหม่
-      };
-    }
 
-    case DELETE_ADDRESS: {
-      const idToDelete = (action as DeleteAddressAction).payload;
-      const targetAddress = state.address.find(a => a.id === idToDelete);
-      
-      // ลบรายการนั้นออก
-      let updatedAddresses = state.address.filter(a => a.id !== idToDelete);
-
-      // (Logic พิเศษ) ถ้าลบตัวที่เป็นค่าเริ่มต้นไป แล้วยังมีที่อยู่เหลืออยู่ ให้ตัวแรกกลายเป็นค่าเริ่มต้นแทน
-      if (targetAddress?.isDefault && updatedAddresses.length > 0) {
-        updatedAddresses = updatedAddresses.map((address, index) => 
-          index === 0 ? { ...address, isDefault: true } : address
-        );
-      }
-
-      return {
-        ...state,
-        address: updatedAddresses,
-      };
-    }
-
-    case SET_DEFAULT_ADDRESS: {
-      const idToDefault = (action as SetDefaultAddressAction).payload;
-      
-      // วนลูปเพื่อเซ็ตตัวที่ ID ตรงกันให้ isDefault เป็น true และตัวอื่นเป็น false
-      const updatedAddresses = state.address.map(address => ({
-        ...address,
-        isDefault: address.id === idToDefault
-      }));
-
-      return {
-        ...state,
-        address: updatedAddresses,
-      };
-    }
-
-    default:
-      return state;
+export const addAddress = createAsyncThunk(
+  "address/addAddress",
+  async (data: Partial<Address>) => {
+    const response = await UserService.addAddress(data);
+    return response;
   }
-};
+);
 
-export default addressReducer;
+export const fetchAllAddresses = createAsyncThunk(
+  "address/fetchAllAddresses",
+  async () => {
+    const response = await UserService.fetchAllAddresses();
+    return response;
+  }
+);
+
+const addressSlice = createSlice({
+  name: "address",
+  initialState: {
+    addresses: [] as Address[],
+    defaultAddress: null as Address | null,
+  },
+  reducers: {},
+});
+
+
+export default addressSlice.reducer;
+
+
+
+
+
+
---
+++ src/types/address.ts
@@ -0,0 +1,7 @@
+export interface Address {
+    id: number;
+    receiverName: string;
+    receiverPhone: string;
+    fullAddress: string;
+    isDefault: boolean;
+}
