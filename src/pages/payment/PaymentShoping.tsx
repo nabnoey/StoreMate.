1cea90844dd4e9e065b39e05a73630e947d3dde0
Ntkfxy <natthaan5710@gmail.com>
2026-04-21 22:28:28 +0700

    update src path

--- src/pages/payment/PaymentShoping.tsx
+++ src/pages/payment/PaymentShoping.tsx
@@ -17,7 +17,7 @@ import { PaymentService } from "../../services/payment.service";
 import { fetchAddressDefault } from "../../redux/address/addressReducer";
 import { addSavedCard } from "../../redux/payment/paymentReducer";
 import { fetchCartThunk } from "../../redux/carts/CartReducer";
-import { PAYMENT_OPTIONS } from "../../constants/paymentOptions"
+import { PAYMENT_OPTIONS } from "../../constants/payment";
 const PaymentShoping = () => {
   const dispatch = useDispatch<AppDispatch>();
   const navigate = useNavigate();
