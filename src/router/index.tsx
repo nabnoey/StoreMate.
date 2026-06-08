e221417e99552115b190c6e745beacd12a71da65
aomezzz <aomezzzz.aomsin@gmail.com>
2026-06-08 18:59:19 +0700

    branch analytic

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -45,6 +45,7 @@ import OrderDetail from "../pages/admin/OrderDetail";
 import UserEdit from "../pages/admin/UserEdit";
 import StoreEdit from "../pages/admin/StoreEdit";
 import AdminNotificationPage from "../pages/admin/Notification";
+import Analytic from "../pages/admin/Analytic";
 
 const router = createBrowserRouter([
   {
@@ -214,6 +215,10 @@ const router = createBrowserRouter([
         element: <Stock />,
       },
       {
+        path: "analytic",
+        element: <Analytic />,
+      },
+      {
         path: "orders",
         element: <Order />,
       },
@@ -246,6 +251,10 @@ const router = createBrowserRouter([
       {
         path: "stock",
         element: <Stock />,
+      },
+      {
+        path: "analytic",
+        element: <Analytic />,
       },
 
       {
