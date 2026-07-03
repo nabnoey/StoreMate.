7bb8303eb3ab50c6b2cae6b797c91d55e551e274
Ntkfxy <natthaan5710@gmail.com>
2026-07-04 04:12:23 +0700

    fix: update manageRefund

--- src/pages/admin/RefundPage.tsx
+++ src/pages/admin/RefundPage.tsx
@@ -12,7 +12,8 @@ import {
 } from "../../redux/moderator/refundReducer";
 import HeaderAdmin from "../../components/admin/HeaderAdmin";
 import { toast } from "react-hot-toast";
-
+// MANAGE ในนี้คือ"รายการที่ต้องเข้าไปจัดการ" เช่น กดอนุมัติคำขอ หรือปฎิเสธ
+// จริงๆใน BE ไม่มีแต่ที่เพิ่มมาเพราะเอาไว้จำกัดการแสดงข้อความในคอลัมน์ การดำเนินการ
 type ModalType = "VIEW" | "MANAGE" | null;
 
 const RefundPage = () => {
